05665858dc94cbb660826551ae6a6fbf94f9bd49
SSARMINU <127767691+SSARMINU@users.noreply.github.com>
2024-05-26 13:09:11 +0530

    Update FoodBox.jsx

--- components/FoodBox.jsx
+++ components/FoodBox.jsx
@@ -1,7 +1,8 @@
 // src/components/FoodBox.js
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
-const FoodBox = ({ food, onAdd }) => {
+const FoodBox = ({ food, onAdd, onReset }) => {
   const [quantity, setQuantity] = useState(1);
 
   const handleQuantityChange = (event) => {
@@ -14,17 +15,17 @@ const FoodBox = ({ food, onAdd }) => {
 
   return (
     <div className="box">
-      <article className="media">
+      <div className="media">
         <div className="media-left">
           <figure className="image is-64x64">
-            <img src={food.image} alt={food.name} />
+            <img src={food.img} alt={food.name} />
           </figure>
         </div>
         <div className="media-content">
           <div className="content">
             <p>
               <strong>{food.name}</strong> <br />
-              <small>{food.calories} cal</small>
+              <small>{food.cal} cal</small>
             </p>
           </div>
         </div>
@@ -46,9 +47,21 @@ const FoodBox = ({ food, onAdd }) => {
             </div>
           </div>
         </div>
-      </article>
+      </div>
+      <button onClick={onReset}>Reset</button>
     </div>
   );
+};
+
+FoodBox.propTypes = {
+  food: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    img: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    cal: PropTypes.number.isRequired,
+  }).isRequired,
+  onAdd: PropTypes.func.isRequired,
+  onReset: PropTypes.func.isRequired,
 };
 
 export default FoodBox;
