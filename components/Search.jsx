8154949d205e1a696ed0ffb2170d445ff9f30a84
SSARMINU <127767691+SSARMINU@users.noreply.github.com>
2024-05-26 13:08:52 +0530

    Update Search.jsx

--- components/Search.jsx
+++ components/Search.jsx
@@ -1,20 +1,31 @@
 // src/components/Search.js
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Search = ({ searchTerm, onSearch }) => {
   return (
-    <div className="field">
-      <div className="control">
+    <div className="field is-grouped">
+      <div className="control is-expanded">
         <input
-          className="input"
+          className="input is-large"
           type="text"
           value={searchTerm}
           onChange={onSearch}
           placeholder="Search for a food"
         />
       </div>
+      <div className="control">
+        <button className="button is-info is-large" onClick={() => {}}>
+          Search
+        </button>
+      </div>
     </div>
   );
+};
+
+Search.propTypes = {
+  searchTerm: PropTypes.string.isRequired,
+  onSearch: PropTypes.func.isRequired,
 };
 
 export default Search;
